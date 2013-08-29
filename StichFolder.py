05624551e6d65dc204e225464902b0b32fdc509d
Ian Foster <mrlanrat@gmail.com>
2013-08-29 16:01:04 -0700

    fixed stuching bug, added pritn statments

--- StichFolder.py
+++ StichFolder.py
@@ -3,13 +3,15 @@ import FixFilenames
 
 def stichVertical():
 
-	for x in range(64):
-		os.system('convert ' + str(x) + '/*.png -append ' + str(x) + '.png')
+    for x in range(64):
+        print "Folder: " + str(x)
+        os.system('convert ' + str(x) + '/*.png -append ' + str(x) + '.png')
 
 def stichHorizontal():
-	FixFilenames.fixTopLevel()
-	os.system('convert *.png -append composite.png')
+    print "Root folder"
+    FixFilenames.fixTopLevel()
+    os.system('convert *.png +append composite.png')
 
 if __name__ == '__main__':
-	stichVertical()
-	stichHorizontal()
+    stichVertical()
+    stichHorizontal()
