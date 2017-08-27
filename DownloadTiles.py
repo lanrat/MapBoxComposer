1af9f4dad0d8ebb55975243985e1226b3ed4e60d
Ian Foster <mrlanrat@gmail.com>
2017-08-26 17:50:09 -0700

    vars

--- DownloadTiles.py
+++ DownloadTiles.py
@@ -2,6 +2,9 @@
 import os
 import time
 
+mapname = "submarine-cable-map-2013"
+ext = "png"
+
 def generateMakefile():
 	z = 6
 	allList = []
@@ -9,11 +12,11 @@ def generateMakefile():
 		os.system('mkdir -p ' + str(x))
 		for y in range(60 / 4):
 			y = y * 4;
-			download1 = ('http://a.tiles.telegeography.com/maps/submarine-cable-map-2013/' + str(z) + '/' + str(x) + '/' + str(y) + '.png > ' + str(x) + '/' + str(y) + '.png') 
-			download2 = ('http://b.tiles.telegeography.com/maps/submarine-cable-map-2013/' + str(z) + '/' + str(x) + '/' + str(y + 1) + '.png > ' + str(x) + '/' + str(y + 1) + '.png')
-			download3 = ('http://c.tiles.telegeography.com/maps/submarine-cable-map-2013/' + str(z) + '/' + str(x) + '/' + str(y + 2) + '.png > ' + str(x) + '/' + str(y + 2) + '.png')
-			download4 = ('http://d.tiles.telegeography.com/maps/submarine-cable-map-2013/' + str(z) + '/' + str(x) + '/' + str(y + 3) + '.png > ' + str(x) + '/' + str(y + 3) + '.png')
-
+			download1 = ('http://a.tiles.telegeography.com/maps/'+ mapname +'/' + str(z) + '/' + str(x) + '/' + str(y    ) + '.' + ext + ' > ' + str(x) + '/' + str(y    ) + '.png') 
+			download2 = ('http://b.tiles.telegeography.com/maps/'+ mapname +'/' + str(z) + '/' + str(x) + '/' + str(y + 1) + '.' + ext + ' > ' + str(x) + '/' + str(y + 1) + '.png')
+			download3 = ('http://c.tiles.telegeography.com/maps/'+ mapname +'/' + str(z) + '/' + str(x) + '/' + str(y + 2) + '.' + ext + ' > ' + str(x) + '/' + str(y + 2) + '.png')
+			download4 = ('http://d.tiles.telegeography.com/maps/'+ mapname +'/' + str(z) + '/' + str(x) + '/' + str(y + 3) + '.' + ext + ' > ' + str(x) + '/' + str(y + 3) + '.png')
+ 
 			downloadCmd = 'wget --user-agent="Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.3) Gecko/2008092416 Firefox/3.0.3" -O - '
 
 			print str(x) + '_' + str(y) + ':'
